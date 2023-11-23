87463a8c21e2252d9f2e0865662f1de3467637be
Roman Nebel <im.roman.nebel@gmail.com>
2023-11-23 23:41:08 +0100

    Fix queue

--- config.js
+++ config.js
@@ -1,18 +1,13 @@
-import dotenv from 'dotenv'
-import path from 'path';
-import { fileURLToPath } from 'url';
-dotenv.config()
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+require("dotenv").config()
 
 const {DIR_PATH, CONFIG_PATH, TILDA_PUB_KEY, TILDA_SECRET, PORT} = process.env
 
-export const configPath = (CONFIG_PATH || __dirname) + '/tilda.conf';
-export const dirPath = (DIR_PATH || __dirname) + '/files';
-
-export const tildaAPI = {
+const configPath = (CONFIG_PATH || __dirname) + '/tilda.conf';
+const dirPath = (DIR_PATH || __dirname + '/files');
+const tildaAPI = {
   getPageFull: `https://api.tildacdn.info/v1/getpagefull/?publickey=${TILDA_PUB_KEY}&secretkey=${TILDA_SECRET}`
 }
+const serverPort = PORT || 8080;
+const timeout = 60 * 60 * 1000
 
-export const serverPort = PORT || 8080;
+module.exports = { configPath, dirPath, tildaAPI, serverPort, timeout }
--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-import fs from 'fs';
-import express from 'express';
-import fetch from 'node-fetch';
-import { NginxConfFile } from 'nginx-conf';
-import { exec } from 'child_process';
-import {configPath, dirPath, tildaAPI, serverPort} from './config.js'
+const fs = require('fs');
+const express = require('express');
+const fetch = require('node-fetch');
+const { NginxConfFile } = require('nginx-conf');
+const { exec } = require('child_process');
+const { configPath, dirPath, tildaAPI, serverPort, timeout } = require('./config')
 
 const server = express();
+
+function queueMachine() {
+  const queue = []
+  let isHold = false
+
+  function next() {
+    return queue.shift()
+  }
+
+  function add(item) {
+    console.log(`Page ${item} was added to queue!`)
+    return queue.push(item)
+  }
+
+  async function start() {
+    setInterval(async() => {
+      const item = queue[0]
+      if (!item || isHold) return 
+      else {
+        isHold = true
+        if (await savePage(item)) {
+        next()
+        isHold = false
+        console.log(`Page ${item} was removed from queue!`)
+        } else {
+          setTimeout(() => isHold = false, timeout)
+        }
+      }
+    }, 1000)
+  }
+
+  start()
+
+  return {add}
+}
 
 async function NginxConf() {
   return new Promise((resolve, reject) => {
@@ -48,18 +83,30 @@ function checkPage(config, pageId) {
   return false
 }
 
-async function savePage(page) {
-  const data = await fetch(tildaAPI.getPageFull + '&pageid=' + page.pageid)
-  .then((response) => response.json())
+async function savePage(pageId) {
+  if (!pageId) return
+  console.log(`Saving page ${pageId}...`)
+  const {status, message, result} = await fetch(tildaAPI.getPageFull + '&pageid=' + pageId)
+  .then(response => response.json())
+  .catch((error) => {
+    console.log(error)
+    return {
+      message: "Server drop request."
+    }
+  });
 
-  console.log("data recieved")
+  if (status !== "FOUND") {
+    console.log(`Page ${pageId} wasn't loaded! `, message)
+    return false
+  }
 
-  const {html, alias} = data.result || {}
+  const {alias, html} = result || {}
+
   const pagePath = alias ? alias.split('/', 2) : null
 
   if (pagePath) {
     const config = await NginxConf()
-    const isCurrentPage = checkPage(config, page.pageid)
+    const isCurrentPage = checkPage(config, pageId)
 
     if (!isCurrentPage) {
       const regStr = '^/' + pagePath[0] + '$'
@@ -68,10 +115,10 @@ async function savePage(page) {
 
       if (parentDirIndex === -1) {
         config.nginx._add('location', `/${pagePath[0]}`)
-        config.nginx.location[result.nginx.location.length-1]._add('rewrite', `^/${alias}$ /page${page.pageid}.html`)
+        config.nginx.location[result.nginx.location.length-1]._add('rewrite', `^/${alias}$ /page${pageId}.html`)
         config.flush();
       } else {
-        config.nginx.location[parentDirIndex]._add('rewrite', `^/${alias}$ /page${page.pageid}.html`)
+        config.nginx.location[parentDirIndex]._add('rewrite', `^/${alias}$ /page${pageId}.html`)
       }
 
       restartNGINX()
@@ -84,18 +131,26 @@ async function savePage(page) {
     });
   }
 
-  const path = `${dirPath}/page${page.pageid}.html`
+  const path = `${dirPath}/page${pageId}.html`
   
   fs.writeFileSync(path, html)
 
-  console.log("page saved")
+  console.log(`Page ${pageId} was saved!`)
+
+  return true
 }
 
-server.get('/webhook', (req, res) => {
-    const {query} = req
-    console.log(query)
+const queue = queueMachine()
+
+server.get('/webhook', async (req, res) => {
+    const { pageid } = req.query
+    if (!pageid) {
+      res.sendStatus(400)
+      return
+    }
+    console.log('New change on Tilda. Page', pageid)
     res.sendStatus(200)
-    savePage(query)
+    queue.add(pageid)
   });
 
   server.listen(serverPort, () => {
