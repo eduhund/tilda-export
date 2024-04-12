ece607efde58fdc7d94e0de25931859719c7e500
Roman Nebel <im.roman.nebel@gmail.com>
2024-04-12 13:35:08 +0200

    Update saving logic

--- index.js
+++ index.js
@@ -1,158 +1,150 @@
-const fs = require('fs');
-const express = require('express');
-const fetch = require('node-fetch');
-const { NginxConfFile } = require('nginx-conf');
-const { exec } = require('child_process');
-const { configPath, dirPath, tildaAPI, serverPort, timeout } = require('./config')
+const fs = require("fs");
+const express = require("express");
+const fetch = require("node-fetch");
+const { NginxConfFile } = require("nginx-conf");
+const { exec } = require("child_process");
+const {
+  configPath,
+  dirPath,
+  tildaAPI,
+  serverPort,
+  timeout,
+} = require("./config");
 
 const server = express();
 
 function queueMachine() {
-  const queue = []
-  let isHold = false
+  const queue = [];
+  let isHold = false;
 
   function next() {
-    return queue.shift()
+    return queue.shift();
   }
 
   function add(item) {
-    console.log(`Page ${item} was added to queue!`)
-    return queue.push(item)
+    console.log(`Page ${item} was added to queue!`);
+    return queue.push(item);
   }
 
   async function start() {
-    setInterval(async() => {
-      const item = queue[0]
-      if (!item || isHold) return 
+    setInterval(async () => {
+      const item = queue[0];
+      if (!item || isHold) return;
       else {
-        isHold = true
+        isHold = true;
         if (await savePage(item)) {
-        next()
-        isHold = false
-        console.log(`Page ${item} was removed from queue!`)
+          next();
+          isHold = false;
+          console.log(`Page ${item} was removed from queue!`);
         } else {
-          setTimeout(() => isHold = false, timeout)
+          setTimeout(() => (isHold = false), timeout);
         }
       }
-    }, 1000)
+    }, 1000);
   }
 
-  start()
+  start();
 
-  return {add}
+  return { add };
 }
 
 async function NginxConf() {
   return new Promise((resolve, reject) => {
     NginxConfFile.create(configPath, function (err, conf) {
       if (err || !conf) {
-          reject()
+        reject();
       }
-      resolve(conf)
-    })
-  })
+      resolve(conf);
+    });
+  });
 }
 
 function restartNGINX() {
   try {
-    const restart = exec('systemctl restart nginx');
+    const restart = exec("systemctl restart nginx");
 
-    restart.stderr.on('data', function (data) {
-      console.log('restart stderr: ' + data);
+    restart.stderr.on("data", function (data) {
+      console.log("restart stderr: " + data);
     });
 
-    restart.stdout.on('data', function (data) {
-      console.log('restart stdout: ' + data);
+    restart.stdout.on("data", function (data) {
+      console.log("restart stdout: " + data);
     });
 
-    restart.on('close', function (code) {
-      console.log('Restart NGINX with the code ' + code);
-    })
+    restart.on("close", function (code) {
+      console.log("Restart NGINX with the code " + code);
+    });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
 function checkPage(config, pageId) {
   for (const location of config.nginx.location || []) {
     for (const rewrite of location.rewrite || []) {
-      if (rewrite._value.includes(pageId)) return true
+      if (rewrite._value.includes(pageId)) return true;
     }
   }
 
-  return false
+  return false;
 }
 
 async function savePage(pageId) {
-  if (!pageId) return
-  console.log(`Saving page ${pageId}...`)
-  const {status, message, result} = await fetch(tildaAPI.getPageFull + '&pageid=' + pageId)
-  .then(response => response.json())
-  .catch((error) => {
-    console.log(error)
-    return {
-      message: "Server drop request."
-    }
-  });
+  if (!pageId) return;
+  console.log(`Saving page ${pageId}...`);
+  const { status, message, result } = await fetch(
+    tildaAPI.getPageFull + "&pageid=" + pageId
+  )
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(error);
+      return {
+        message: "Server drop request.",
+      };
+    });
 
   if (status !== "FOUND") {
-    console.log(`Page ${pageId} wasn't loaded! `, message)
-    return false
+    console.log(`Page ${pageId} wasn't loaded! `, message);
+    return false;
   }
 
-  const {alias, html} = result || {}
+  const { alias, html } = result || {};
 
-  const pagePath = alias ? alias.split('/', 2) : null
+  const pagePath = pageId === "5234768" ? "index" : alias;
 
-  if (pagePath) {
-    const config = await NginxConf()
-    const isCurrentPage = checkPage(config, pageId)
-
-    if (!isCurrentPage) {
-      const regStr = '^/' + pagePath[0] + '$'
-      const reg = new RegExp(regStr)
-      const parentDirIndex = config.nginx.location.findIndex((loc) => reg.test(loc._value))
-
-      if (parentDirIndex === -1) {
-        config.nginx._add('location', `/${pagePath[0]}`)
-        config.nginx.location[result.nginx.location.length-1]._add('rewrite', `^/${alias}$ /page${pageId}.html`)
-        config.flush();
-      } else {
-        config.nginx.location[parentDirIndex]._add('rewrite', `^/${alias}$ /page${pageId}.html`)
-      }
-
-      restartNGINX()
-    }
+  if (!pagePath) {
+    console.log(`Page ${pageId} have no alias!`);
+    return false;
   }
 
   if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath, { recursive: true }, (err) => {
+    fs.mkdirSync(dirPath + "/" + pagePath, { recursive: true }, (err) => {
       if (err) throw err;
     });
   }
 
-  const path = `${dirPath}/page${pageId}.html`
-  
-  fs.writeFileSync(path, html)
+  const path = `${dirPath}/${pagePath}.html`;
 
-  console.log(`Page ${pageId} was saved!`)
+  fs.writeFileSync(path, html);
 
-  return true
+  console.log(`Page ${pageId} was saved!`);
+
+  return true;
 }
 
-const queue = queueMachine()
+const queue = queueMachine();
 
-server.get('/webhook', async (req, res) => {
-    const { pageid } = req.query
-    if (!pageid) {
-      res.sendStatus(400)
-      return
-    }
-    console.log('New change on Tilda. Page', pageid)
-    res.sendStatus(200)
-    queue.add(pageid)
-  });
+server.get("/webhook", async (req, res) => {
+  const { pageid } = req.query;
+  if (!pageid) {
+    res.sendStatus(400);
+    return;
+  }
+  console.log("New change on Tilda. Page", pageid);
+  res.sendStatus(200);
+  queue.add(pageid);
+});
 
-  server.listen(serverPort, () => {
-  console.log(`Server listening on port ${serverPort}`)
-})
+server.listen(serverPort, () => {
+  console.log(`Server listening on port ${serverPort}`);
+});
